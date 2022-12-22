3a2210ca836417357dcf6577c95773e2d953179b
Ieuan <ieuan@Ieuans-MacBook-Pro.local>
2022-12-22 23:11:04 +0700

    add .env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+// try @next/font package for font optimization
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
