b9b24e59c7d6c9ab5f4b1bf8fd10923da7fcdfc6
ieuanign <97420341+ieuanign@users.noreply.github.com>
2022-12-23 22:14:39 +0700

    dockerized boilerplate for production (#1)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'page.ts']
-}
+	reactStrictMode: true,
+	pageExtensions: ["page.tsx", "page.ts"],
+	output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
